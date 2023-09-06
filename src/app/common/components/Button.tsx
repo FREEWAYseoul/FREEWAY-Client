90658015a8a9813484e15fb14fed1763070c03bf
bbung <tjd123123@naver.com>
2023-09-06 16:05:00 +0900

    feat: styorybook add

---
+++ src/app/common/components/Button.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+type Props = {
+  text: string;
+};
+
+const Button = ({ text }: Props) => {
+  return <button>{text}</button>;
+};
+
+export default Button;
