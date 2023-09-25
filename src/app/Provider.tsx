d6886ac2c1e39f1f49ba4dc96d90770e7f022d96
bbung <tjd123123@naver.com>
2023-09-26 01:14:09 +0900

    feat: google-analytics add

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -6,6 +6,9 @@ import { PropsWithChildren, useState } from 'react';
 import SearchContextProvider from '@/common/context/SearchContext';
 
 const Provider = ({ children }: PropsWithChildren) => {
+  /**
+   * instance가 변경되어 cache유지되지 않아 state로 관리
+   */
   const [queryClient] = useState(() => new QueryClient());
 
   return (
