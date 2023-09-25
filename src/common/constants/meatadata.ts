86241ed2f010a167b7f3724505979d36b2208413
bbung <tjd123123@naver.com>
2023-09-25 14:45:20 +0900

    feat: meta-data add

---
+++ src/common/constants/meatadata.ts
@@ -0,0 +1,12 @@
+const META_DATA = {
+  title: '프리웨이 - FREEWAY',
+  description:
+    '서울시 지하철을 이용하는 교통약자들을 위한 실시간 지하철 정보를 제공하는 프리웨이 지하철 이용이 FREE 해질 수 있는 경험을 해보세요!',
+  type: 'website',
+  url: 'https://freeway-seoul.vercel.app',
+  images:
+    'https://github.com/SeSACTHON-FREE-WAY/free-way-front/assets/77668478/70ebde05-ea71-4aba-bc69-8547f69022df',
+  locale: 'ko_KR',
+} as const;
+
+export default META_DATA;
