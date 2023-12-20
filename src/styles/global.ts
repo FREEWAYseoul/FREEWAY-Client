753937e16d8d002826d56fa118b73693fedd87ef
bbung <tjd123123@naver.com>
2023-12-20 17:45:14 +0900

    style: safari font fix
    - local font remove
    - cdn font add

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,18 @@ const GlobalStyle = createGlobalStyle`
 
   ${reset}
 
+  :root{
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  }
+
+  input{
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+      'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji',
+      'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+  }
+
   *{
     box-sizing: border-box;
     -ms-text-size-adjust: 100%; /* 2 */
@@ -18,7 +30,6 @@ const GlobalStyle = createGlobalStyle`
     overflow: hidden;
     font-style: normal;
     overscroll-behavior: none;
-
   }
 
 
